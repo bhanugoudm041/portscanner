102c38ebdcc028af5a276268d650cee759f4d99c
Bhanu <bhanu@kali.kali>
2022-01-21 06:43:56 -0500

    a new port scanner written in python

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # portscanner
 a simple port scanner written in python to identify open ports. specially designed for CTF
+its a simple python program to scan open ports on system
+# installation
+clone the repository using command
+git clone https://github.com/bhanugoudm041/portscanner.git
+
+then give it excutable permission using command 
+chmod +x portscanner.py
+
+# running 
+./portscanner.py (or) python3 portscanner.py
+
+once the program run you will see a welcome banner and it prompts to enter ip addresss 
+Enter the ip address you want to scan
+then it prompts to enter port range then give a port range ex:1-1000 if you want to scan a single port enter port number ex:80
+
+# thank you ......
---
+++ portscanner.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+import nmap
+
+print("  ____   ___   ____  ______       _____   __   ____  ____   ____     ___  ____   ")
+print(" |    \ /   \ |    \|      |     / ___/  /  ] /    ||    \ |    \   /  _]|    \  ")
+print(" |  o  )     ||  D  )      |    (   \_  /  / |  o  ||  _  ||  _  | /  [_ |  D  ) ")
+print(" |   _/|  O  ||    /|_|  |_|     \__  |/  /  |     ||  |  ||  |  ||    _]|    /  ")
+print(" |  |  |     ||    \  |  |       /  \ /   \_ |  _  ||  |  ||  |  ||   [_ |    \  ")
+print(" |  |  |     ||     \ |  |       \    \     ||  |  ||  |  ||  |  ||     ||     \ ")
+print(" |__|   \___/ |__|\_| |__|        \___|\____||__|__||__|__||__|__||_____||__|\_| ")                                                                             
+print("******************************developed by bhanugoud***************************")
+print("************************Use it for educational purpose only********************")
+
+ip=str(input('Enter the ip address you want to scan : '))
+port=str(input('Enter the ports range you want scan Ex:1-1000 or single port 80 : '))
+nm=nmap.PortScanner()
+ports=nm.scan(hosts=ip,ports=port)
+print(ports['scan'][ip]['tcp'])
