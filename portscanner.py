935b3c068f5cedd724a4d36454e0e74819fda2f8
bhanugoudm041 <bhanugoud041@gmail.com>
2022-09-22 06:04:15 -0400

    bug fix

--- portscanner.py
+++ portscanner.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-import nmap
-
+import nmap3
 print("  ____   ___   ____  ______       _____   __   ____  ____   ____     ___  ____   ")
 print(" |    \ /   \ |    \|      |     / ___/  /  ] /    ||    \ |    \   /  _]|    \  ")
 print(" |  o  )     ||  D  )      |    (   \_  /  / |  o  ||  _  ||  _  | /  [_ |  D  ) ")
@@ -13,6 +12,9 @@ print("************************Use it for educational purpose only**************
 
 ip=str(input('Enter the ip address you want to scan : '))
 port=str(input('Enter the ports range you want scan Ex:1-1000 or single port 80 : '))
-nm=nmap.PortScanner()
-ports=nm.scan(hosts=ip,ports=port)
-print(ports['scan'][ip]['tcp'])
+print("*****************************************************************************")
+nm=nmap3.NmapHostDiscovery()
+ports=nm.nmap_portscan_only(ip,"-p"+port)
+ports_open=((ports[ip])["ports"])
+for i in ports_open:
+	print("Port: ",i['portid'],"--> ","State: ",i["state"])
